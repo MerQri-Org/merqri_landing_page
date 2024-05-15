39ca7f0080f96b32abe998a121bd66fc8286404f
Francis <fhrancorey99@gmail.com>
2024-05-15 21:58:34 +0100

    feat: created a react app using vite

---
+++ merqri_landing_page/src/components/index.js
@@ -0,0 +1,13 @@
+import AboutUs from "./AboutUs"
+import Footer from "./Footer"
+import Hero from "./Hero"
+import Navbar from "./Navbar"
+import Services from "./Services"
+
+export {
+    AboutUs,
+    Footer,
+    Hero,
+    Navbar,
+    Services
+}
