4190f73894e49215fc912b9a361a602badfc628e
Programmer-Shams <ssdeen313@gmial.com>
2024-06-08 15:08:14 +0100

    api joinwaitlist

--- merqri_landing_pages/src/sections/Subscribe.jsx
+++ merqri_landing_pages/src/sections/Subscribe.jsx
@@ -1,8 +1,12 @@
 import { SlArrowRightCircle } from "react-icons/sl";
 import Button from "../components/Button";
-import React from "react";
+import { handleJoinWaitList } from "../apis/JoinWaitlist";
+import React, { useState } from "react";
 
 const Subscribe = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
   return (
     <section className="bg-[#D9D9D9]" id="contact">
       <div className="px-5 md:px-20 py-[10rem] flex flex-col md:flex-row gap-10 md:gap-0">
@@ -11,45 +15,52 @@ const Subscribe = () => {
         </div>
 
         <div className="md:w-[60%] flex flex-col gap-10">
-        <div className="flex flex-col gap-5">
-          <h1 className="text-[3rem] text-primary font-[600]">
-            Moving Made Easy: <br /> We'll Be There Soon!
-          </h1>
-          <p className="text-sm md:w-[80%]">
-            Get ready for a smooth move with Merqri! Our app is coming soon,
-            packed with information and resources to make your move a breeze.
-          </p>
-          <p className="text-sm md:w-[70%]">
-            In the meantime, sign up below to be notified when our app goes live
-            and receive exclusive offers!
-          </p>
-        </div>
-        <div className="flex flex-col gap-10">
-          <div className="flex flex-col gap-5 w-[100%]">
-            <label htmlFor="" className="text-secondary text-[2.5rem]">
-              Your Name
-            </label>
-            <input
-              type="text"
-              className="border-b-4 border-secondary focus:outline-none text-xl bg-[#D9D9D9]"
-            />
+          <div className="flex flex-col gap-5">
+            <h1 className="text-[3rem] text-primary font-[600]">
+              Moving Made Easy: <br /> We'll Be There Soon!
+            </h1>
+            <p className="text-sm md:w-[80%]">
+              Get ready for a smooth move with Merqri! Our app is coming soon,
+              packed with information and resources to make your move a breeze.
+            </p>
+            <p className="text-sm md:w-[70%]">
+              In the meantime, sign up below to be notified when our app goes
+              live and receive exclusive offers!
+            </p>
           </div>
-          <div className="flex flex-col gap-5 w-[100%]">
-            <div className="flex justify-between">
-              <label htmlFor="" className="text-secondary text-[2.5rem]">
-                Your Email
+          <div className="flex flex-col gap-10">
+            <div className="flex flex-col gap-5 w-[100%]">
+              <label htmlFor="" className="text-secondary text-[1.6rem]">
+                Your Name
               </label>
-              <button className="text-secondary font-semibold py-2 px-4 rounded-lg cursor-pointer text-xs md:text-sm">
-                <SlArrowRightCircle size={50} />
-              </button>
+              <input
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                type="text"
+                className="border-b-4 border-secondary focus:outline-none text-xl bg-[#D9D9D9]"
+              />
             </div>
-            <input
-              type="text"
-              className="border-b-4 border-secondary focus:outline-none text-xl bg-[#D9D9D9]"
-            />
+            <div className="flex flex-col gap-5 w-[100%]">
+              <div className="flex justify-between">
+                <label htmlFor="" className="text-secondary text-[1.6rem]">
+                  Your Email
+                </label>
+                <button 
+                onClick={(event) => handleJoinWaitList(name, email, setMessage, event)}
+                className="text-secondary font-semibold py-2 px-4 rounded-lg cursor-pointer text-xs md:text-sm">
+                  <SlArrowRightCircle size={40} />
+                </button>
+              </div>
+              <input
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                type="text"
+                className="border-b-4 border-secondary focus:outline-none text-xl bg-[#D9D9D9]"
+              />
+            </div>
+            {message && <div className="text-center text-[1.5rem] mt-10 text-primary font-semibold">{message}</div>}
           </div>
         </div>
-      </div>
       </div>
     </section>
   );
--- merqri_landing_pages/src/sections/WaitlistJoin.jsx
+++ merqri_landing_pages/src/sections/WaitlistJoin.jsx
@@ -1,34 +1,46 @@
-import React from "react";
+import { handleJoinWaitList } from "../apis/JoinWaitlist";
+import React, { useState } from "react";
 import { SlArrowRightCircle } from "react-icons/sl";
 
 const WaitlistJoin = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
   return (
     <section className="bg-white px-5 md:px-20 py-20 flex flex-col items-center">
       <div className="flex flex-col gap-10 w-full md:px-20">
         <div className="flex flex-col gap-5">
-          <label htmlFor="" className="text-secondary text-[1.5rem] md:text-[2.5rem]">
+          <label htmlFor="" className="text-secondary text-[1.5rem] md:text-[2rem]">
             Your Name
           </label>
           <input
             type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
             className=" border-b-4 border-secondary focus:outline-none text-xl"
           />
         </div>
         <div className="flex flex-col gap-5">
           <div className="flex justify-between items-center">
-            <label htmlFor="" className="text-secondary text-[1.5rem] md:text-[2.5rem]">
+            <label htmlFor="" className="text-secondary text-[1.5rem] md:text-[2rem]">
               Your Email
             </label>
-            <button className="text-secondary font-semibold py-2 px-4 rounded-lg cursor-pointer text-xs md:text-sm">
-              <SlArrowRightCircle size={50} />
+            <button  
+            onClick={(event) => handleJoinWaitList(name, email, setMessage, event)}
+            className="text-secondary font-semibold py-2 px-4 rounded-lg cursor-pointer text-xs md:text-sm">
+              <SlArrowRightCircle size={40} />
             </button>
           </div>
           <input
             type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             className="border-b-4 border-secondary focus:outline-none text-xl"
           />
         </div>
       </div>
+      {message && <div className="text-center text-[1.5rem] mt-10 text-primary font-semibold">{message}</div>}
       <div className="text-center flex justify-center mt-10">
         <p className="text-[2rem] text-secondary md:w-[53%] text-center">
           We are just starting our journey, but we promise that it will be cool!
