2853ff454af6467ddaa83287a28ab78287288468
Daniel Anoka <anokadaniel@gmail.com>
2024-07-29 15:38:29 +0100

    mobile-navbar

--- merqri_landing_pages/src/components/Navbar.jsx
+++ merqri_landing_pages/src/components/Navbar.jsx
@@ -1,85 +1,107 @@
+import { useState } from "react";
 import { navLinks } from "../constants";
-import { IoMenu } from "react-icons/io5";
-import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "../components/ui/sheet";
+import { IoMenu, IoClose } from "react-icons/io5";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+import { Link as ScrollLink } from "react-scroll";
 
 const Navbar = () => {
-  return (
-    <nav className="bg-white shadow-md">
-      <div className="hidden sm:block">
-        <div className="flex py-6 items-center justify-between navbar mx-20">
-          <Link to="/">
-            <img src="/images/logo.png" alt="logo" className="h-8" />
-          </Link>
-          <div className="flex items-center">
-            <ul className="flex gap-6">
-              <Link to="/" className="text-gray-600 font-semibold">
-                Home
-              </Link>
-              {navLinks.map((link, index) => (
-                <li key={index} className="-ml-3">
-                  <a
-                    href={`#${link.id}`}
-                    className="text-xs md:text-[15px] font-semibold text-gray-600 hover:text-black cursor-pointer"
-                  >
-                    {link.title}
-                  </a>
-                </li>
-              ))}
-              <Link to="/partnership" className="text-gray-600 font-semibold">
-                Partners
-              </Link>
-            </ul>
-          </div>
-          <Link
-            to="/waitlist"
-            className={`ml-4 bg-primary text-white font-semibold py-2 px-4 rounded-lg cursor-pointer text-xs md:text-sm`}
-          >
-            Join The Waitlist 
-          </Link>
-        </div>
-      </div>
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-      {/* Mobile View */}
-      <div className="sm:hidden bg-white shadow-md p-4 w-full flex items-center justify-between">
-        <img src="/images/logo.png" alt="Merqri Logo" className="h-8" />
-        <Sheet>
-          <SheetTrigger className="">
-            <IoMenu size={28} className="text-gray-600" />
-          </SheetTrigger>
-          <SheetContent className="bg-white pt-14">
-            <SheetHeader>
-              <SheetTitle className="flex">
-                <img src="/images/logo.png" alt="logo" className="h-8" />
-              </SheetTitle>
-              <SheetDescription className="flex justify-start">
-                <div className="mt-10">
-                  <ul className="flex flex-col text-left gap-10">
-                    {navLinks.map((link, index) => (
-                      <li key={index} className="">
-                        <a
-                          href={link.href}
-                          className="font-semibold hover:text-black cursor-pointer text-gray-600"
-                        >
-                          {link.title}
-                        </a>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              </SheetDescription>
-            </SheetHeader>
-          </SheetContent>
-        </Sheet>
-      </div>
-    </nav>
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <>
+      <nav className="bg-white shadow-md fixed top-0 left-0 right-0 w-full z-50">
+        <div className="hidden sm:block">
+          <div className="flex py-6 items-center justify-between navbar mx-20">
+            <Link to="/">
+              <img src="/images/logo.png" alt="logo" className="h-8" />
+            </Link>
+            <div className="flex items-center">
+              <ul className="flex gap-6">
+                <Link to="/" className="text-gray-600 font-semibold">
+                  Home
+                </Link>
+                {navLinks.map((link, index) => (
+                  <li key={index} className="-ml-3">
+                    <ScrollLink
+                      to={link.id}
+                      smooth={true}
+                      duration={500}
+                      className="text-xs md:text-[15px] font-semibold text-gray-600 hover:text-black cursor-pointer"
+                    >
+                      {link.title}
+                    </ScrollLink>
+                  </li>
+                ))}
+                <Link to="/partnership" className="text-gray-600 font-semibold">
+                  Partners
+                </Link>
+              </ul>
+            </div>
+            <Link
+              to="/waitlist"
+              className="ml-4 bg-primary text-white font-semibold py-2 px-4 rounded-lg cursor-pointer text-xs md:text-sm"
+            >
+              Join The Waitlist
+            </Link>
+          </div>
+        </div>
+
+        {/* Mobile View */}
+        <div className="sm:hidden bg-white shadow-md p-4 w-full flex items-center justify-between relative">
+          <Link to="/">
+            <img src="/images/logo.png" alt="Merqri Logo" className="h-8" />
+          </Link>
+          <button onClick={toggleMenu}>
+            {isMenuOpen ? (
+              <IoClose size={28} className="text-gray-600" />
+            ) : (
+              <IoMenu size={28} className="text-gray-600" />
+            )}
+          </button>
+          {isMenuOpen && (
+            <motion.div
+              initial={{ x: '-100%' }}
+              animate={{ x: 0 }}
+              exit={{ x: '-100%' }}
+              transition={{ type: 'tween', duration: 0.3 }}
+              className="absolute top-full left-0 right-0 bg-white bg-opacity-90 shadow-lg mt-1 p-3 z-50"
+            >
+              <ul className="flex flex-col text-left gap-4">
+                {navLinks.map((link, index) => (
+                  <li key={index}>
+                    <ScrollLink
+                      to={link.id}
+                      smooth={true}
+                      duration={500}
+                      className="font-semibold hover:text-black cursor-pointer text-gray-600"
+                      onClick={() => setIsMenuOpen(false)}
+                    >
+                      {link.title}
+                    </ScrollLink>
+                  </li>
+                ))}
+                <li>
+                <Link
+                  to="/partnership"
+                  className="font-semibold hover:text-black cursor-pointer text-gray-600"
+                  onClick={() => setIsMenuOpen(false)}
+                >
+                  Partners
+                </Link>
+                </li>
+              </ul>
+            </motion.div>
+          )}
+        </div>
+      </nav>
+
+     
+    </>
   );
 };
 
--- merqri_landing_pages/src/sections/Hero.jsx
+++ merqri_landing_pages/src/sections/Hero.jsx
@@ -1,25 +1,32 @@
 import { Link } from "react-router-dom";
 import { GoArrowUpRight } from "react-icons/go";
-import { AiFillSafetyCertificate, } from "react-icons/ai";
+import { AiFillSafetyCertificate } from "react-icons/ai";
 import { MdVpnKey } from "react-icons/md";
 import { FaMoneyBills } from "react-icons/fa6";
 
-
 const HeroSection = () => {
-  const imageLeft = "/Vector.png"; 
-  const imageRight = "/right.png"; 
+  const imageLeft = "/Vector.png";
+  const imageRight = "/right.png";
   return (
-    <div className="flex flex-col items-center justify-center">
-      <div className="container mx-auto px-4 py-16 text-center font-lato">
-      <img src={imageLeft} className="absolute left-0 top-32 h-4/6 opacity-60 hidden md:block" alt="Left Image " />
-      <img src={imageRight} className="absolute right-40 top-80 h-16 opacity-60 hidden md:block" alt="Right Image" />
+    <div className="flex flex-col items-center justify-center" >
+      <div className="container mx-auto px-4 pt-28 pb-16 text-center font-lato">
+        <img
+          src={imageLeft}
+          className="absolute left-0 top-32 h-4/6 opacity-60 hidden md:block"
+          alt="Left Image "
+        />
+        <img
+          src={imageRight}
+          className="absolute right-40 top-80 h-16 opacity-60 hidden md:block"
+          alt="Right Image"
+        />
         <h1 className="text-2xl md:text-4xl font-bold text-secondary mb-4">
           Seamless <span className="text-primary">Relocation</span> Awaits!
         </h1>
         <p className="text-sm text-secondary text-center md:text-xl ">
-          Your smooth move starts here. Unlock the best moving experience  <br /> with
-          our curated list of top-notch moving companies. Let&apos;s <br /> make your
-          relocation stress-free and efficient!
+          Your smooth move starts here. Unlock the best moving experience <br />
+          with our curated list of top-notch moving companies. Let&apos;s <br />
+          make your relocation stress-free and efficient!
         </p>
         <button className="bg-primary hover:bg-secondary text-white font-bold py-2 rounded-lg px-4 mt-8">
           <Link to="/waitlist" className="flex items-center gap-1">
@@ -29,25 +36,25 @@ const HeroSection = () => {
       </div>
 
       <div className="flex flex-row gap-2 md:gap-14 items-center text-secondary text-xs md:text-sm font-lato mb-0 md:mb-8">
-      <div className="flex items-center">
-        <div className="rounded-full bg-secondary text-white flex items-center justify-center w-6 h-6">
-          <MdVpnKey className="text-sm" />
+        <div className="flex items-center">
+          <div className="rounded-full bg-secondary text-white flex items-center justify-center w-6 h-6">
+            <MdVpnKey className="text-sm" />
+          </div>
+          <p className="ml-2">Comprehensive Management</p>
         </div>
-        <p className="ml-2">Comprehensive Management</p>
-      </div>
-      <div className="flex items-center">
-        <div className="rounded-full bg-secondary text-white flex items-center justify-center w-6 h-6">
-          <AiFillSafetyCertificate className="text-sm" />
+        <div className="flex items-center">
+          <div className="rounded-full bg-secondary text-white flex items-center justify-center w-6 h-6">
+            <AiFillSafetyCertificate className="text-sm" />
+          </div>
+          <p className="ml-2">Safety</p>
         </div>
-        <p className="ml-2">Safety</p>
-      </div>
-      <div className="flex items-center">
-        <div className="rounded-full bg-secondary text-white flex items-center justify-center w-6 h-6">
-          <FaMoneyBills className="text-sm" />
+        <div className="flex items-center">
+          <div className="rounded-full bg-secondary text-white flex items-center justify-center w-6 h-6">
+            <FaMoneyBills className="text-sm" />
+          </div>
+          <p className="ml-2">Reasonable Price</p>
         </div>
-        <p className="ml-2">Reasonable Price</p>
       </div>
-    </div>
     </div>
   );
 };
